0e53c2d8f5d6f9fb0405b9184b1dc38822b25e8a
Ilnar Khasanov <37439026+ilnarkhasanov@users.noreply.github.com>
2025-11-22 00:44:07 +0300

    Add Continuous Integration workflow (#30)

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def summary(start_commit: str, end_commit: str) -> None:
     changes = [
         "Add mypy to the project",
         "Remove old Docker Compose configuration",
-        "Add feature of multiplying 2 numbers"
+        "Add feature of multiplying 2 numbers",
     ]
 
     print(f"Start commit: {start_commit}")
@@ -16,9 +16,8 @@ def summary(start_commit: str, end_commit: str) -> None:
 
     print("Summary of changes:")
     for change_index, change in enumerate(changes):
-        print(f"{change_index+1}. {change}")
+        print(f"{change_index + 1}. {change}")
 
 
 if __name__ == "__main__":
     app()
-
