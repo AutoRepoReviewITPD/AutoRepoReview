6910bdec7593ed2182e29176dcda4c54501a30d4
ilnarkhasanov <4sitescarp@gmail.com>
2025-11-22 00:12:34 +0300

    format main.py

--- app/main.py
+++ app/main.py
@@ -3,16 +3,12 @@ import typer
 app = typer.Typer()
 
 
-
-
-
-
 @app.command()
 def summary(start_commit: str, end_commit: str) -> None:
     changes = [
         "Add mypy to the project",
         "Remove old Docker Compose configuration",
-        "Add feature of multiplying 2 numbers"
+        "Add feature of multiplying 2 numbers",
     ]
 
     print(f"Start commit: {start_commit}")
@@ -20,9 +16,8 @@ def summary(start_commit: str, end_commit: str) -> None:
 
     print("Summary of changes:")
     for change_index, change in enumerate(changes):
-        print(f"{change_index+1}. {change}")
+        print(f"{change_index + 1}. {change}")
 
 
 if __name__ == "__main__":
     app()
-
