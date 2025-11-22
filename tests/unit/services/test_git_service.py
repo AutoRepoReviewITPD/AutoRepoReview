53678310454c36d6efcaa1f778fabe8177339feb
ilnarkhasanov <4sitescarp@gmail.com>
2025-11-22 17:58:37 +0300

    add unit test for test_pull

--- tests/unit/services/test_git_service.py
+++ tests/unit/services/test_git_service.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from services.git_service import GitService
 
 
@@ -6,3 +7,13 @@ def test_get_diff(
 ) -> None:
     diff = git_service.get_diff(".", "HEAD", "HEAD~1")
     assert isinstance(diff, str)
+
+
+def test_clone(
+    git_service: GitService,
+) -> None:
+    repo = "https://github.com/torvalds/linux"
+    clone_path = "/tmp/linux_repo"
+    with patch.object(git_service, "_call_os") as mock_call_os:
+        git_service.clone(repo, clone_path)
+        mock_call_os.assert_called_once_with(f"git clone {repo} {clone_path}")
