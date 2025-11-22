423267b2eb60433ee61d9678405efd53f3c16c1f
ilnarkhasanov <4sitescarp@gmail.com>
2025-11-22 16:49:56 +0300

    add test_agent

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
+from unittest.mock import Mock
+from langchain_core.language_models import LanguageModelLike
 from services.git_service import GitService
 from services.summarize_service import SummarizeService
+from agents.agent import Agent
 
 
 @pytest.fixture
@@ -11,3 +14,13 @@ def summarize_service() -> SummarizeService:
 @pytest.fixture
 def git_service() -> GitService:
     return GitService()
+
+
+@pytest.fixture
+def mock_model() -> Mock:
+    return Mock(spec=LanguageModelLike)
+
+
+@pytest.fixture
+def agent(mock_model: Mock) -> Agent:
+    return Agent(model=mock_model, tools=[])
