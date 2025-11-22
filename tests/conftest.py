860c0256e2845cd86fa1fd641097ac7163ad21a4
ilnarkhasanov <4sitescarp@gmail.com>
2025-11-22 19:23:26 +0300

    use single conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+from unittest.mock import Mock
 import pytest
 
+from agents.agent import Agent
 from services.git_service import GitService
 from services.summarize_service import SummarizeService
+from langchain_core.language_models import LanguageModelLike
 
 
 @pytest.fixture
@@ -12,3 +15,13 @@ def summarize_service() -> SummarizeService:
 @pytest.fixture
 def git_service() -> GitService:
     return GitService()
+
+
+@pytest.fixture
+def mock_model() -> Mock:
+    return Mock(spec=LanguageModelLike)
+
+
+@pytest.fixture
+def agent(mock_model: Mock) -> Agent:
+    return Agent(model=mock_model, tools=[])
