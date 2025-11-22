ff09680f2ee24a231ca6d1bce9f9489b0f3a27d3
ilnarkhasanov <4sitescarp@gmail.com>
2025-11-22 19:44:26 +0300

    add test_qas001_1

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,12 @@ from services.summarize_service import SummarizeService
 from langchain_core.language_models import LanguageModelLike
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def summarize_service() -> SummarizeService:
     return SummarizeService()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def git_service() -> GitService:
     return GitService()
 
@@ -25,3 +25,11 @@ def mock_model() -> Mock:
 @pytest.fixture
 def agent(mock_model: Mock) -> Agent:
     return Agent(model=mock_model, tools=[])
+
+
+@pytest.fixture(scope="session")
+def cloned_repo(git_service: GitService) -> str:
+    repo = "https://github.com/ilnarkhasanov/AiToHuman"
+    clone_path = "/tmp/repo"
+    git_service.clone(repo, clone_path)
+    return clone_path
