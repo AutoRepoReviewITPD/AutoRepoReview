bb0b31ff39750da3c71acd3030e30e63834e3ca0
ilnarkhasanov <4sitescarp@gmail.com>
2025-11-22 18:06:51 +0300

    format

--- app/services/git_service.py
+++ app/services/git_service.py
@@ -8,6 +8,6 @@ class GitService:
     def get_diff(self, path: str, start_commit: str, end_commit: str) -> str:
         os.chdir(path)
         return self._call_os(f"git diff {start_commit} {end_commit}")
-    
+
     def clone(self, repo_url: str, clone_path: str) -> None:
         self._call_os(f"git clone {repo_url} {clone_path}")
