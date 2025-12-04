ccdc274b69a3872179b938794552ae3354a160fd
alenaartemyeva <a.artemyeva@along.pw>
2025-12-04 12:56:15 +0300

    Move SummarizeService initialization under summary command

--- app/__main__.py
+++ app/__main__.py
@@ -9,12 +9,12 @@ from .services.summarize_service import SummarizeService
 app = typer.Typer()
 
 git_service = GitService()
-summarize_service = SummarizeService()
 
 
 @app.command()
 def summary(path: str, start_commit: str, end_commit: str) -> None:
     """Creates a summary of changes between two commits."""
+    summarize_service = SummarizeService()
     try:
         diff = git_service.get_diff(path, start_commit, end_commit)
         print(summarize_service.summarize(diff))
